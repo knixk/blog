ecb1302f5bd5e19a77baec01bac6bbd45916b3b1
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-10-01 21:36:59 +0530

    changes

--- app/controllers/notepad.rb
+++ app/controllers/notepad.rb
@@ -42,6 +42,9 @@ wasted time will never come back type regret
  x body instinct
  x wash clothes
  x wash body 
+ x disorientation
+ x high bp
+ x eyes worst
 
  2. u don't decide to watch it
 
@@ -53,9 +56,87 @@ wasted time will never come back type regret
  x have to control it
  x no pleasure, but the biggest pleasure is having no regrets!
 
+if u want to make ur chote, u need to set an example first.
+ayenge lekin you don't listen to them or actively feed them.
 
-What happened happened. Now
-Focus on work.
+u want, but also. aisa nahi hota
+okay?
+if u want clarity in life. don't
+otherwise
+  brain fog
 
-You know how u got this right? U did this for amazing period. Do it again.
-Honestly. THere is no pleasure in that.
+somehow ur saved. even tho u didn't do anything.
+
+
+
+### Action items for today:
+o - Do something for boards fix it anything idk do it.
+w - Insights have been updated.
+o - make the qa document.
+o - Draft the campaign.
+
+
+
+
+
+# action items for today:
+
+1. x - complete testing of campaign and journey on phone.
+2. o - setup test account for hdw5. (blocker android studio isn't working. can't able to find)
+3. o - what about ct error, (why it wasn't working)
+4.
+
+
+ram lena padegi hmm le skta hun
+
+ek ka ticket bhi ayega paise toh mil jayenge
+
+lekin kaam toh kuch kiya hi nahi?
+
+aisa nahi hai
+
+
+
+# Action items:
+
+1. Test everything for hdw5
+2. make qa sheet for HDW5
+3. Add all the notifics this time.
+4. make the same for hdw5
+
+
+
+
+
+variants
+1. only engish
+2. with translation 
+
+
+
+
+
+
+
+
+// pet m infection, pet m dard, can't sit for long or even sleep properly
+// have seen many doctors
+// have done a lot of test, now ct scan and endoscopy have asked.
+// paise will be given
+// when i will be back, i might catch an infection again, i'll have to be careful
+// **** also when can I be full time, 6 mahine ho chuke hai... kab tak internship karunga
+// 
+
+aaj firse typhoid test aur malaria test karaya hai
+
+
+ct ke 5100 rupe maang rahe hai,
+rent dena hai,,,
+
+wapas kab ayega? kaam karega lekin wapas pura theek hoke hi ayega,
+
+ aapne toh bola tha?
+
+ 
+
+
